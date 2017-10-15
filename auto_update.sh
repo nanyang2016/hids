a63d5860c4587c73c5d922de59cafc7064d5209f
nanyang <690615723@qq.com>
2017-10-15 17:46:08 +0800

    update Update time:20171015 17:46:08  Update comment:update auto_update.sh

--- auto_update.sh
+++ auto_update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 time=`date +"%Y%m%d %H:%M:%S"`
-content=""
+content=$1
 update_content="Update time:"$time"  Update comment:"$content
 git add *
 git commit -m "update $update_content"
