6c3cc88f2b6ad38ea0ce984e06755526a6dd346a
nanyang <690615723@qq.com>
2017-11-08 00:16:28 +0800

    update Update time:20171108 00:16:27  Update comment:

--- my_hids/brute_force/bin/test.sh
+++ my_hids/brute_force/bin/test.sh
@@ -1,26 +1,14 @@
 #!/bin/bash
 
-#line=`wc -l /var/log/auth.log `
-#line=8431
-#str=`sed -n "$line,\$p" /var/log/auth.log`
 
-#str=`sed -n "$line,\$p" /var/log/auth.log`
-#echo "$str"
-
-line=`cat /var/log/auth.log|head`
-
-
-#IFS_bak=$IFS
-#IFS="\n"
-i=1
-cat /var/log/auth.log|head > tmp_file
-#for one_line in `cat /var/log/auth.log|head`
-while read one_line
+OLD_IFS=$IFS
+IFS=$'\n'
+for one_line in $(cat /var/log/auth.log|head)
 do
 	echo $i"$one_line"
 	((i++))
-done < tmp_file
-#IFS=$IFS_bak
+done
+IFS=$OLD_IFS
 
 
 
